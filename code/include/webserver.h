38777f77f7f90376353919e04d4bd18cc495efa4
Huang Zhujiang <alinpk@outlook.com>
2023-11-26 19:36:21 +0800

    establish webserver project

---
+++ code/include/webserver.h
@@ -0,0 +1,19 @@
+#ifdef WEB_SERVER_H
+#define WEB_SERVER_H
+
+constexpr int MAX_FD = 65536;
+constexpr int MAX_EVENT_NUMBER = 10000;
+constexpr int TIMEOUT_S = 5;
+
+class WebServer
+{
+public:
+    WebServer();
+    ~WebServer();
+
+    void int()
+
+private:
+};
+
+#endif
